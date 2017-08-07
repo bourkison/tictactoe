2eaad7acc055457bbf7eb0424e9065995bb96800
bourkison <harrisonbourke97@gmail.com>
2017-08-07 16:52:43 +1000

    Added AI, however there is still a bug when there is a draw

--- js/script.js
+++ js/script.js
@@ -59,10 +59,12 @@ let newTurn = function(xPos, yPos) {
   // Update the HTML
   updateHTML();
 
-  // Now, if we need to check if we're playing one player or two player, and if we're playing two player we need to come up with the computer's turn.
-  if (!twoPlayer) {
+  // Now, if we need to check if we're playing one player or two player, and if we're playing two player AND its o's turn, we need the ai to make a move.
+  if (!twoPlayer && !xTurn && !gameWon) {
     aiTurn();
   } // if
+
+  turns++;
 } // newTurn
 
 
@@ -162,8 +164,7 @@ let turns = 0;
 
 // Use jQuery to check for a click
 $(".box").click(function() {
-  if (turns < 9) {
-    turns++;
+  if (turns < 9 && !gameWon) {
     let index = this.id;
 
     // Calculate the x and y position based off of the id.
@@ -178,6 +179,7 @@ $(".box").click(function() {
   }
 });
 
+
 $("#onep").click(function() {
   $("table").css("display", "block");
   $("#buttons").css("display", "none");
@@ -187,8 +189,331 @@ $("#onep").click(function() {
 $("#twop").click(function() {
   $("table").css("display", "block");
   $("#buttons").css("display", "none");
+  twoPlayer = true;
 });
 
-let aiTurn = function() {
 
+
+let aiTurn = function() {
+  // Let's set up an array of the move to make, where the 0th element is x pos and the 1st element is y pos
+  let moveToMake = [];
+
+  // First let's check to see if the computer can win
+  moveToMake = checkForTwoInRow("O");
+  // If moveToMake is not equal to null, then we have gotten a value and should input that.
+  if (moveToMake !== null) {
+    newTurn(moveToMake[0], moveToMake[1]);
+    return;
+  }
+
+  // Now let's check to see if the computer needs to block off a player's victory.
+  moveToMake = checkForTwoInRow("X");
+  if (moveToMake !== null) {
+    newTurn(moveToMake[0], moveToMake[1]);
+    return;
+  }
+
+
+  // Now let's check to see if the computer can fork the player.
+  moveToMake = checkForFork("O");
+  if (moveToMake !== null) {
+    newTurn(moveToMake[0], moveToMake[1]);
+    return;
+  }
+
+  // Now let's check to see if the player can fork, and if so let's block it.
+  moveToMake = checkForFork("X");
+  if (moveToMake !== null) {
+    newTurn(moveToMake[0], moveToMake[1]);
+    return;
+  }
+
+  // Now we need to check if the center position is available, and if so we'll choose that one.
+  if (gameBoard[1][1] === " ") {
+    newTurn(1, 1);
+    return;
+  }
+
+
+  // Now let's check to see if the player has chosen a corner and if so we will pick the opposite one.
+  moveToMake = checkCorner("X");
+  if (moveToMake !== null) {
+    newTurn(moveToMake[0], moveToMake[1]);
+    return;
+  }
+
+  // Now let's just check to see if theres an empty corner available.
+  moveToMake = checkEmptyCorner();
+  if (moveToMake !== null) {
+    newTurn(moveToMake[0], moveToMake[1]);
+    return;
+  }
+
+  // Now let's just choose an empty side.
+  moveToMake = checkEmptySide();
+  newTurn(moveToMake[0], moveToMake[1]);
+  return;
 } // aiTurn
+
+let checkForTwoInRow = function(s) {
+  // First let's check horizontally.
+  for (let i = 0; i < gameBoard.length; i++) {
+    // Check if on this row (i) the 0th and 1st index are equal OR the 1st and 2nd index are equal OR the 0th and 2nd are equal. If so, return the best one to place.
+    if (gameBoard[i][0] === gameBoard[i][1]) {
+      // Now check if either the 1st are equal to the input
+      if (gameBoard[i][0] === s) {
+        // Now check if our desired spot is empty.
+        if (gameBoard[i][2] === " ") {
+          return [2, i];
+        } // if
+      } // if
+    } // if
+    else if (gameBoard[i][1] === gameBoard[i][2]) {
+      if (gameBoard[i][1] === s) {
+        if (gameBoard[i][0] === " ") {
+          return [0, i];
+        }
+      } // if
+    } // else if
+    else if (gameBoard[i][0] === gameBoard[i][2]) {
+      if (gameBoard[i][0] === s) {
+        if (gameBoard[i][1] === " ") {
+          return [1, i];
+        }
+      } // if
+    } // else if
+  } // for
+
+  // Now check vertically
+  for (let i = 0; i < gameBoard.length; i++) {
+    if (gameBoard[0][i] === gameBoard[1][i]) {
+      // Now check if either the 1st are equal to the input
+      if (gameBoard[0][i] === s) {
+        if (gameBoard[2][i] === " ") {
+          return [i, 2];
+        }
+      } // if
+    } // if
+    else if (gameBoard[1][i] === gameBoard[2][i]) {
+      if (gameBoard[1][i] === s) {
+        if (gameBoard[0][i] === " ") {
+          return [i, 0];
+        }
+      }
+    }
+    else if (gameBoard[0][i] === gameBoard[2][i]) {
+      if (gameBoard[0][i] === s) {
+        if (gameBoard[1][i] === " ") {
+          return [i, 1];
+        }
+      }
+    }
+  }
+
+
+  // Now let's check diagonally from top-left to bottom-right.
+  if (gameBoard[0][0] === gameBoard[1][1]) {
+    if (gameBoard[0][0] === s) {
+      if (gameBoard[2][2] === " ") {
+        return [2, 2];
+      }
+    }
+  }
+  else if (gameBoard[0][0] === gameBoard[2][2]) {
+    if (gameBoard[0][0] === s) {
+      if (gameBoard[1][1] === " ") {
+        return [1, 1];
+      }
+    }
+  }
+  else if (gameBoard[1][1] === gameBoard[2][2]) {
+    if (gameBoard[1][1] === s) {
+      if (gameBoard[0][0] === " ") {
+        return [0, 0];
+      }
+    }
+  }
+
+  // Now lets check diagonally from top-right to bottom-left.
+  if (gameBoard[0][2] === gameBoard[1][1]) {
+    if (gameBoard[1][1] === s) {
+      if (gameBoard[2][0] === " ") {
+        return [0, 2];
+      }
+    }
+  }
+  else if (gameBoard[1][1] === gameBoard[2][0]) {
+    if (gameBoard[1][1] === s) {
+      if (gameBoard[0][2] === " ") {
+        return [2, 0];
+      }
+    }
+  }
+  else if (gameBoard[0][2] === gameBoard[2][0]) {
+    if (gameBoard[0][2] === s) {
+      if (gameBoard[1][1] === " ") {
+        return [1, 1];
+      }
+    }
+  }
+
+  // If none of these return anything, just return null.
+  return null;
+
+}
+
+
+
+let checkForFork = function(s) {
+  // First let's check to see if the top-left corner is equal to s
+  if (gameBoard[0][0] === s) {
+    // Now let's check to see if the top-left corner is equal to any other corners.
+    if (gameBoard[0][0] === gameBoard[0][2]) {
+      // Now let's check to see if either of the remaining corners are available.
+      if (gameBoard[2][2] === " ") {
+        return [2, 2];
+      }
+
+      if (gameBoard[2][0] === " ") {
+        return [0, 2];
+      }
+
+    }
+    else if (gameBoard[0][0] === gameBoard[2][2]) {
+      if (gameBoard[0][2] === " ") {
+        return [2, 0];
+      }
+
+      if (gameBoard[2][0] === " ") {
+        return [0, 2];
+      }
+    }
+    else if (gameBoard[0][0] === gameBoard[2][0]) {
+      if (gameBoard[0][2] === " ") {
+        return [2, 0];
+      }
+
+      if (gameBoard[2][2] === " ") {
+        return [2, 2];
+      }
+    }
+  }
+
+  // Now let's check for the top-right corner - keeping in mind that we don't have to check the top-left corner again. As we've already checked every possible outcome.
+  if (gameBoard[0][2] === s) {
+    if (gameBoard[0][2] === gameBoard[2][2]) {
+      if (gameBoard[0][0] === " ") {
+        return[0, 0];
+      }
+
+      if (gameBoard[2][0] === " ") {
+        return [0, 2];
+      }
+    }
+    else if (gameBoard[0][2] === gameBoard[2][0]) {
+      if (gameBoard[0][0] === " ") {
+        return [0, 0];
+      }
+
+      if (gameBoard [2][2] === " ") {
+        return [2, 2];
+      }
+    }
+  }
+
+  // Finally we just need to compare the bottom-right corner with the bottom-left corner as we've compared every other possible option.
+  if (gameBoard[2][2] === s) {
+    if (gameBoard[2][2] === gameBoard[2][0]) {
+      if (gameBoard[0][0] === " ") {
+        return [0, 0];
+      }
+
+      if (gameBoard[0][2] === " ") {
+        return [2, 0];
+      }
+    }
+  }
+
+  // If none of these return anything, just return null.
+  return null;
+
+}
+
+
+let checkCorner = function(s) {
+
+  // Top left corner
+  if (gameBoard[0][0] === s && gameBoard[2][2] === " ") {
+    return [2, 2];
+  }
+
+  // Top right corner
+  if (gameBoard[0][2] === s && gameBoard[2][0] === " ") {
+    return [0, 2];
+  }
+
+  // Bottom right corner
+  if (gameBoard[2][2] === s && gameBoard[0][0] === " ") {
+    return [0, 0];
+  }
+
+  // Bottom left corner
+  if (gameBoard[2][0] === s && gameBoard[0][2] === " ") {
+    return [2, 0];
+  }
+
+  // Else return null
+  return null;
+}
+
+
+let checkEmptyCorner = function() {
+
+  // Top-left
+  if (gameBoard[0][0] === " ") {
+    return [0, 0];
+  }
+
+  //Top-right
+  if (gameBoard[0][2] === " ") {
+    return [2, 0];
+  }
+
+  // Bottom-right
+  if (gameBoard[2][2] === " ") {
+    return [2, 2];
+  }
+
+  // Bottom-left
+  if (gameBoard[2][0] === " ") {
+    return [0, 2];
+  }
+
+  return null;
+
+}
+
+
+let checkEmptySide = function() {
+
+  // Top-middle
+  if (gameBoard[0][1] === " ") {
+    return [1, 0];
+  }
+
+  // Right-middle
+  if (gameBoard[1][2] === " ") {
+    return [2, 1];
+  }
+
+  // Bottom-middle
+  if(gameBoard[2][1] === " ") {
+    return [1, 2];
+  }
+
+  //Left-middle
+  if (gameBoard[1][0] === " ") {
+    return[0, 1];
+  }
+
+}
